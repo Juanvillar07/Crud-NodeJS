a06bbb97e8667b2bf7ec9fd98fb808da7380858d
login-node <jvillar747@gmail.com>
2024-01-30 11:39:27 -0500

    deploy

--- api/index.js
+++ api/index.js
@@ -4,30 +4,30 @@ const routerApi = require('./routes');
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/errorHandler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 //Darle acceso a origenes en especifico con cors
-const whitelist = ['http://localhost:8080', 'https://myapp.com'];
-const options = {
-  origin: (origin, callback) =>{
-    if(whitelist.includes(origin)){
-      callback(null, true);
-    } else{
-      callback(new Error('No permitido'));
-    }
-  }
-}
+// const whitelist = ['http://localhost:8080', 'https://myapp.com'];
+// const options = {
+//   origin: (origin, callback) =>{
+//     if(whitelist.includes(origin)){
+//       callback(null, true);
+//     } else{
+//       callback(new Error('No permitido'));
+//     }
+//   }
+// }
 
-app.use(cors(options));
+app.use(cors());
 
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hola, mi server en Express');
 });
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/api/nueva-ruta', (req, res) => {
   res.send('Hola, esta es una nueva ruta');
 });
 
